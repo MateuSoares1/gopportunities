f8740d35844ad99f50e5d5cb450a703e156bdf90
kokujin <mateus.soares@wconnect.com.br>
2024-01-21 17:55:01 -0300

    feat: criando estrutura de logging

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
-import "github.com/MateuSoares1/gopportunities/router"
+import (
+	"github.com/MateuSoares1/gopportunities/config"
+	"github.com/MateuSoares1/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	//Initialize config
+	err := config.Init()
 
+	if err != nil {
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
 	//Initialize router
 	router.Initialize()
 }
