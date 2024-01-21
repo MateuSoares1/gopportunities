7627079d5a2001e857e9960660f6b3c4b79e47a3
kokujin <mateus.soares@wconnect.com.br>
2024-01-21 18:39:42 -0300

    feat: initialize CRUD

--- schemas/opening.go
+++ schemas/opening.go
@@ -1,6 +1,8 @@
 package schemas
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -11,5 +13,18 @@ type Opening struct {
 	Location string
 	Remote   bool
 	Link     string
-	Salaty   int64
+	Salary   int64
+}
+
+type OpeningResponse struct {
+	ID        uint      `json: "id"`
+	CreatedAt time.Time `json: "createdAt"`
+	UpdatedAt time.Time `json: "updatedAt"`
+	DeletedAt time.Time `json: "updatedAt", omitempty`
+	Role      string    `json: "role"`
+	Company   string    `json: "company"`
+	Location  string    `json: "location"`
+	Remote    bool      `json: "remote"`
+	Link      string    `json: "link"`
+	Salary    int64     `json: "salary"`
 }
